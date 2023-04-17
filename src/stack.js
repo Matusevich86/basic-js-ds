29db94a53b775898321115b3d75ec8fa6029ef83
Matusevich86 <alex.matusevich86@gmail.com>
2023-04-17 10:53:02 +0300

    add stack

--- src/stack.js
+++ src/stack.js
@@ -13,20 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor()
+      {
+          this.items = [];
+      }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(element) {
+      this.items.push(element);
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+      if (this.items.length == 0)
+          return "isEmpty";
+      return this.items.pop();
+    }
+
+    peek() {
+      return this.items[this.items.length - 1];
+    }
 }
 
 module.exports = {
